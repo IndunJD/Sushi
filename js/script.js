39227d531258887ce17001b987cf42df8e1c78e1
InduniJD <indunijd@gmail.com>
2024-04-23 14:13:26 +0530

    mobile navbar fixed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-import AOS from "aos";
-import "aos/dist/aos.css";
+import AOS from "aos"
+import "aos/dist/aos.css"
 
 // init AOS animation
 AOS.init({
-    duration: 1000,
-    offset: 100,
-});
+  duration: 1000,
+  offset: 100,
+})
+
+window.toggleMenu = function () {
+  const fullMenu = document.getElementById("full-menu").style.display = "block"
+  fullMenu.classList.toggle("show-menu")
+  console.log("Clicked")
+}
